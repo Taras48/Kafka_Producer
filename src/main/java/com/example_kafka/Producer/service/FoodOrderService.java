4ce6af486f8c5118417ff27a60e612128133e3ec
TKukharyuk <TKukharyuk@sportmasterlab.net>
2026-02-23 13:39:19 +0300

    Initial commit

---
+++ src/main/java/com/example_kafka/Producer/service/FoodOrderService.java
@@ -0,0 +1,22 @@
+package com.example_kafka.Producer.service;
+
+import com.example_kafka.Producer.model.FoodOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FoodOrderService {
+
+    private final Producer producer;
+
+    @Autowired
+    public FoodOrderService(Producer producer) {
+        this.producer = producer;
+    }
+
+    public String createFoodOrder(FoodOrder foodOrder) {
+        return producer.sendMessage(foodOrder);
+    }
+}
---
+++ src/main/java/com/example_kafka/Producer/service/Producer.java
@@ -0,0 +1,34 @@
+package com.example_kafka.Producer.service;
+
+import com.example_kafka.Producer.model.FoodOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+@Slf4j
+@Component
+public class Producer {
+    @Value("${topic.name}")
+    private String orderTopic;
+
+    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public Producer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public String sendMessage(FoodOrder foodOrder){
+        String orderAsMessage = objectMapper.writeValueAsString(foodOrder);
+        kafkaTemplate.send(orderTopic, orderAsMessage);
+
+        log.info("food order produced {}", orderAsMessage);
+
+        return "message sent";
+    }
+}
